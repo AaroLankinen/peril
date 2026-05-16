b5f7f21cec68f37b3eb8ff41b90c3ab10706f12b
Aaro Lankinen <aaro.lankinen@gmail.com>
2026-05-16 15:48:11 +0300

    Server now starts with AMQP connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connectionString := "amqp://guest:guest@localhost:5672/"
 	fmt.Println("Starting Peril server...")
+	fmt.Println("Using connection string:", connectionString)
+	AMQPConnection, error := amqp.Dial(connectionString)
+	if error != nil {
+		panic(error)
+	}
+	defer AMQPConnection.Close()
+	defer fmt.Println("Peril server is shutting down...")
+	fmt.Println("Peril server is running...")
+	// Wait for user input to exit
+	fmt.Println("Press Enter to exit...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
